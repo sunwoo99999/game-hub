b26feb68fae678eb8078e2dda33d939721e90153
swk <sunwoo99999@gmail.com>
2025-01-14 10:15:14 -0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ChakraProvider>
+  <ChakraProvider>
+    <React.StrictMode>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>
+    </React.StrictMode>
+  </ChakraProvider>
 );
